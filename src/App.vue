3a30b990494a6e41e54906a867e9532309e742ea
BG-Guy <53979740+BG-Guy@users.noreply.github.com>
2022-04-12 17:07:27 +0300

    sync

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template class="app">
+  <!-- <AppHeader /> -->
+  <Hero />
+  <!-- <Resume /> -->
+  <Portfolio />
+</template>
+
+<script>
+import AppHeader from "./components/app-header.vue";
+import Hero from "./components/hero.vue";
+import Resume from "./components/resume.vue";
+import Portfolio from "./components/portfolio.vue";
+export default {
+  components: {
+    // AppHeader,
+    Hero,
+    Portfolio,
+    // Resume,
+  },
+
+  data() {},
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/app-footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+    <div class="logo-container"></div>
+    <nav class="nav-links">
+      <ul>
+        <li></li>
+      </ul>
+    </nav>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
