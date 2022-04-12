f010388196057785a3bae15353348bb7615cb191
BG-Guy <53979740+BG-Guy@users.noreply.github.com>
2022-04-12 17:35:06 +0300

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template class="app">
   <AppHeader />
   <Homepage />
-  <Footer />
+  <AppFooter />
 </template>
 
 <script>
 import AppHeader from "./components/app-header.vue";
 import Homepage from "./components/homepage.vue";
+import AppFooter from "./components/app-footer.vue";
 export default {
   components: {
     AppHeader,
     Homepage,
-    Footer,
+    AppFooter,
   },
 
   data() {},
