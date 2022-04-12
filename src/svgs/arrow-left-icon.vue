a4ffe722b529ed5dfbe17ff10e4b242ec512b0a8
BG-Guy <53979740+BG-Guy@users.noreply.github.com>
2022-04-12 17:09:34 +0300

    sync

---
+++ src/svgs/arrow-left-icon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg viewBox="0 0 32 32" class="icon icon-arrow-left" aria-hidden="true">
+    <path
+      d="M26.025 14.496l-14.286-.001 6.366-6.366L15.979 6 5.975 16.003 15.971 26l2.129-2.129-6.367-6.366h14.29z"
+    />
+  </svg>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.icon {
+  height: 20px;
+  width: 20px;
+  cursor: pointer;
+}
+</style>
